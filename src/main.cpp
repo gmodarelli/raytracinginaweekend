c8c982c6d81fa6a33c456801b09bfa23e1f4bd17
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2018-08-07 10:11:09 +0200

    Replace the Ray class with a Ray struct

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,28 @@
 #include "vec3.h"
 #include "ray.h"
 
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
-  vec3 oc = r.origin() - center;
-  float a = dot(r.direction(), r.direction());
-  float b = 2.0 * dot(oc, r.direction());
+float hit_sphere(const vec3& center, float radius, const ray& r) {
+  vec3 oc = r.origin - center;
+  float a = dot(r.direction, r.direction);
+  float b = 2.0 * dot(oc, r.direction);
   float c = dot(oc, oc) - radius * radius;
-  float discriminant = b * b - 5 * a * c;
-  return (discriminant > 0);
+  float discriminant = b * b - 4 * a * c;
+  if (discriminant < 0) {
+    return -1.0;
+  } else {
+    return (-b - sqrt(discriminant)) / (2.0 * a);
+  }
 }
 
 vec3 color(const ray& r) {
-  if (hit_sphere(vec3(0.0, 0.0, -1.0), 0.5, r))
-    return vec3(1.0, 0.0, 0.0);
+  float t = hit_sphere(vec3(0.0, 0.0, -1.0), 0.5, r);
+  if (t > 0.0) {
+    vec3 N = unit_vector(ray_point_at_parameter(r, t) - vec3(0, 0, -1));
+    return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+  }
 
-  vec3 unit_direction = unit_vector(r.direction());
-  float t = 0.5 * (unit_direction.y() + 1.0);
+  vec3 unit_direction = unit_vector(r.direction);
+  t = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
@@ -29,12 +36,14 @@ int main() {
   vec3 horizontal(4.0, 0.0, 0.0);
   vec3 vertical(0.0, 2.0, 0.0);
   vec3 origin(0.0, 0.0, 0.0);
+  ray r;
+  r.origin = origin;
 
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
       float u = float(i) / float(nx);
       float v = float(j) / float(ny);
-      ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+      r.direction = lower_left_corner + u * horizontal + v * vertical;
       vec3 col = color(r);
       int ir = int(255.99 * col.r());
       int ig = int(255.99 * col.g());
--- src/ray.h
+++ src/ray.h
@@ -2,24 +2,17 @@
 #define RAY_H
 #include "vec3.h"
 
-class ray
-{
-  public:
-    ray() {}
-    ray(const vec3& origin, const vec3& direction) {
-      m_Origin = origin; 
-      m_Direction = direction;
-    }
-
-    vec3 origin() const { return m_Origin; }
-    vec3 direction() const { return m_Direction; }
-    // This function returns a point along the 3D line defined by the ray,
-    // starting from its m_Origin towards its m_Direction.
-    // Positive values of t will return points "in front" of the ray, while
-    // negative values will return points "behind" the ray.
-    vec3 point_at_parameter(float t) const { return m_Origin + t * m_Direction; }
-
-    vec3 m_Origin;
-    vec3 m_Direction;
+struct ray {
+  vec3 origin;
+  vec3 direction;
 };
+
+// This function returns a point along the 3D line defined by the ray,
+// starting from its m_Origin towards its m_Direction.
+// Positive values of t will return points "in front" of the ray, while
+// negative values will return points "behind" the ray.
+const vec3 ray_point_at_parameter(const ray& ray, float t) {
+  return ray.origin + t * ray.direction; 
+}
+
 #endif // RAY_H
