f31e4201dbdb553027181d94ebbb7a889ad993c9
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2018-08-08 22:11:35 +0200

    Add a random scene

--- src/main.cpp
+++ src/main.cpp
@@ -34,37 +34,69 @@ vec3 color(sphere spheres[], int n, const ray& r, int depth) {
   }
 }
 
+void random_scene(int& spheres_count, sphere* spheres) {
+  spheres[0].center = vec3(0, -1000, 0);
+  spheres[0].radius = 1000;
+  spheres[0].mat_ptr = new lambertian(vec3(0.5, 0.5, 0.5));
+
+  int i = 1;
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      float choose_mat = drand48();
+      vec3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+      if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+        i += 1;
+        spheres[i].center = center;
+        spheres[i].radius = 0.2;
+
+        if (choose_mat < 0.8) { // diffuse
+          spheres[i].mat_ptr = new lambertian(vec3(drand48() * drand48(), drand48() * drand48(), drand48() * drand48()));
+        }
+        else if (choose_mat < 0.95) { // metal
+          spheres[i].mat_ptr = new metal(vec3(0.5 * (1 + drand48()), 0.5 * (1 + drand48()), 0.5 * (1 + drand48())), 0.5 * drand48());
+        }
+        else {
+          spheres[i].mat_ptr = new dielectric(1.5);
+        }
+      }
+    }
+  }
+
+  i += 1;
+  spheres[i].center = vec3(0, 1, 0);
+  spheres[i].radius = 1.0;
+  spheres[i].mat_ptr = new dielectric(1.5);
+
+  i += 1;
+  spheres[i].center = vec3(-4, 1, 0);
+  spheres[i].radius = 1.0;
+  spheres[i].mat_ptr = new lambertian(vec3(0.4, 0.2, 0.1));
+
+  i += 1;
+  spheres[i].center = vec3(4, 1, 0);
+  spheres[i].radius = 1.0;
+  spheres[i].mat_ptr = new metal(vec3(0.7, 0.6, 0.5), 0.0);
+  
+  spheres_count = i;
+}
+
 int main() {
-  int nx = 400;
-  int ny = 200;
+  int nx = 1200;
+  int ny = 800;
   // Samples per pixel
-  int ns = 100;
+  int ns = 20;
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-  vec3 lookfrom(3, 3, 2);
-  vec3 lookat(0, 0, -1);
-  float dist_to_focus = (lookfrom - lookat).length();
-  float aperture = 2.0;
+  vec3 lookfrom(13, 2, 3);
+  vec3 lookat(0, 0, 0);
+  float dist_to_focus = 10.0;
+  float aperture = 0.1;
 
   camera cam = default_camera(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus);
 
-  sphere spheres[5];
-  spheres[0].center = vec3(0, 0, -1);
-  spheres[0].radius = 0.5;
-  spheres[0].mat_ptr = new lambertian(vec3(0.1, 0.2, 0.5));
-  spheres[1].center = vec3(0, -100.5, -1);
-  spheres[1].radius = 100;
-  spheres[1].mat_ptr = new lambertian(vec3(0.8, 0.8, 0.0));
-  spheres[2].center = vec3(1, 0, -1);
-  spheres[2].radius = 0.5;
-  spheres[2].mat_ptr = new metal(vec3(0.8, 0.6, 0.2), 0.0);
-  spheres[3].center = vec3(-1, 0, -1);
-  spheres[3].radius = 0.5;
-  spheres[3].mat_ptr = new dielectric(1.5);
-  spheres[4].center = vec3(-1, 0, -1);
-  spheres[4].radius = -0.45;
-  spheres[4].mat_ptr = new dielectric(1.5);
-
+  int spheres_count = 500;
+  sphere spheres[spheres_count + 1];
+  random_scene(spheres_count, spheres);
 
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
@@ -73,7 +105,7 @@ int main() {
         float u = float(i + drand48()) / float(nx);
         float v = float(j + drand48()) / float(ny);
         ray r = camera_get_ray(cam, u, v);
-        col += color(spheres, 5, r, 0);
+        col += color(spheres, spheres_count, r, 0);
       }
 
       col /= float(ns);
